141f15b666bf5ce28156b0eb6e769da923425eb9
Arhizmei <arhizmei@gmail.com>
2023-09-23 17:56:39 +0300

    second part

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,22 @@
 import java.util.SimpleTimeZone
 
 fun main() {
-    val a = "Hello"
-    val b = a.getThirdLetter()
-    println(b)
+    var arr = arrayOf(1,2,3,4,5,6)
+    arr.swap(1,3)
+    arr.print()
+
 }
 
-fun String.getThirdLetter(): Char {
-   return this[1]
+fun Array<Int>.swap(i1:Int, i2:Int) {
+
+    val el1 = this[i1]
+    val el2 = this[i2]
+    this[i1]  = el2
+    this[i2]= el1
+}
+
+fun Array<Int>.print(){
+    for (i in this){
+        println(i)
+    }
 }
