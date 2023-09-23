470bfe683122a1f1f8dae239fe8aa974cd30bcd5
Arhizmei <arhizmei@gmail.com>
2023-09-23 22:16:15 +0300

    3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,13 @@
 import java.util.SimpleTimeZone
 
 fun main() {
-    var arr = arrayOf(1,2,3,4,5,6)
-    arr.swap(1,3)
+    var arr = arrayOf("a", "b", "c")
+    arr.swap(1,2)
     arr.print()
 
 }
 
-fun Array<Int>.swap(i1:Int, i2:Int) {
+fun <T> Array<T>.swap(i1:Int, i2:Int) {
 
     val el1 = this[i1]
     val el2 = this[i2]
@@ -15,7 +15,7 @@ fun Array<Int>.swap(i1:Int, i2:Int) {
     this[i2]= el1
 }
 
-fun Array<Int>.print(){
+fun <T> Array<T>.print(){
     for (i in this){
         println(i)
     }
